6c7d4f2390117d4d552c38a82540fb21ffea3521
Conan Torres <conantorreswf@gmail.com>
2024-11-30 21:27:57 -0300

    feat: ajustes nas datas

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function EditTask({ show, handleClose, task, handleSave }) {
+    const formatDate = (date) => {
+        const [day, month, year] = date.split("/");
+        return `${year}-${month}-${day}`;
+    };
+
     const [description, setDescription] = useState(task.description);
     const [category, setCategory] = useState(task.category);
-    const [limitDate, setLimitDate] = useState(task.limit_date);
+    const [limitDate, setLimitDate] = useState(formatDate(task.limit_date));
+
+    useEffect(() => {
+        if (show) {
+            setDescription(task.description);
+            setCategory(task.category);
+            setLimitDate(formatDate(task.limit_date));
+        }
+    }, [show, task]);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = { description, category, limit_date: limitDate };
         handleSave({ ...task, ...formData });
+        handleClose();
+    };
+
+    const handleModalClose = () => {
+        setDescription("");
+        setCategory("");
+        setLimitDate("");
         handleClose();
     };
 
@@ -18,7 +38,7 @@ export default function EditTask({ show, handleClose, task, handleSave }) {
                 <div className="modal-content">
                     <div className="modal-header bg-secondary">
                         <h5 className="modal-title">Editar Tarefa</h5>
-                        <button type="button" className="close btn btn-danger btn-sm ms-auto" data-dismiss="modal" aria-label="Close" onClick={handleClose}>
+                        <button type="button" className="close btn btn-danger btn-sm ms-auto" data-dismiss="modal" aria-label="Close" onClick={handleModalClose}>
                             <span aria-hidden="true">X</span>
                         </button>
                     </div>
