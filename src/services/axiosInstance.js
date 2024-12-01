0e4ffaad901f2ad6eb89680e1daf9c03a4945b06
Conan Torres <conantorreswf@gmail.com>
2024-11-30 23:42:25 -0300

    fix: corrige .env

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: process.env.BASE_URL,
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
